bab9b11407d7f93e95d60c503cf40eb3efa0ed04
Rahul Singh <official.rahul.email@gmail.com>
2025-10-14 11:52:14 +0530

    1. template literals

---
+++ ep01/templateLiteral.js
@@ -0,0 +1,9 @@
+// Template Literals
+const age = 33;
+const fullName = "Rahul Singh";
+
+console.log(`Hello Mr. ${fullName} your age is ${age}`)
+
+const a = 10;
+const b = 20;
+console.log(`Sum of ${a} and ${b} is ${a+b}`)
